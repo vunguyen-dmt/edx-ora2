cf7b75cef88e531f545b7ebf3362dad14694fd93
Vu Nguyen <vu.nguyen@dmttek.com>
2024-02-01 12:12:19 +0000

    build

--- openassessment/xblock/static/dist/openassessment-editor-tinymce.js
+++ openassessment/xblock/static/dist/openassessment-editor-tinymce.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9f178f80778ef11c1723";
+/******/ 	var hotCurrentHash = "6c13a248e10b8dd6e0c1";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
