2d1d2e59600e16e00fba2bcd763ab655d5786d56
Vu Nguyen <vu.nguyen@dmttek.com>
2026-07-14 00:41:32 +0700

    set default rubrics

--- openassessment/xblock/utils/defaults.py
+++ openassessment/xblock/utils/defaults.py
@@ -136,55 +136,95 @@ Nội dung câu hỏi.
 
 DEFAULT_RUBRIC_CRITERIA = [
     {
-        'name': "Đánh giá tổng thể",
-        'label': "Đánh giá tổng thể",
-        'prompt': "Đánh giá tổng thể bài nộp",
+        'name': "Điểm bài làm",
+        'label': "Điểm bài làm",
+        'prompt': "Điểm bài làm",
         'order_num': 0,
-        'feedback': 'optional',
+        'feedback': 'disabled',
         'options': [
             {
-                'order_num': 0, 'points': 0, 'name': '0', 'label': '0',
-                'explanation': """0"""  # nopep8
+                'order_num': 0, 'points': 0, 'name': '0  điểm', 'label': '0  điểm',
+                'explanation': '0 điểm'
             },
             {
-                'order_num': 1, 'points': 1, 'name': '1', 'label': '1',
-                'explanation': """1"""  # nopep8
+                'order_num': 1, 'points': 5, 'name': '0.5 điểm', 'label': '0.5 điểm',
+                'explanation': '5% tổng điểm'
             },
             {
-                'order_num': 2, 'points': 2, 'name': '2', 'label': '2',
-                'explanation': """2"""  # nopep8
+                'order_num': 2, 'points': 10, 'name': '1.0 điểm', 'label': '1.0 điểm',
+                'explanation': '10% tổng điểm'
             },
             {
-                'order_num': 3, 'points': 3, 'name': '3', 'label': '3',
-                'explanation': """3"""  # nopep8
+                'order_num': 3, 'points': 15, 'name': '1.5 điểm', 'label': '1.5 điểm',
+                'explanation': '15% tổng điểm'
             },
             {
-                'order_num': 4, 'points': 4, 'name': '4', 'label': '4',
-                'explanation': """4"""  # nopep8
+                'order_num': 4, 'points': 20, 'name': '2.0 điểm', 'label': '2.0 điểm',
+                'explanation': '20% tổng điểm'
             },
             {
-                'order_num': 5, 'points': 5, 'name': '5', 'label': '5',
-                'explanation': """5"""  # nopep8
+                'order_num': 5, 'points': 25, 'name': '2.5 điểm', 'label': '2.5 điểm',
+                'explanation': '25% tổng điểm'
             },
             {
-                'order_num': 6, 'points': 6, 'name': '6', 'label': '6',
-                'explanation': """6"""  # nopep8
+                'order_num': 6, 'points': 30, 'name': '3.0 điểm', 'label': '3.0 điểm',
+                'explanation': '30% tổng điểm'
             },
             {
-                'order_num': 7, 'points': 7, 'name': '7', 'label': '7',
-                'explanation': """7"""  # nopep8
+                'order_num': 7, 'points': 35, 'name': '3.5 điểm', 'label': '3.5 điểm',
+                'explanation': '35% tổng điểm'
             },
             {
-                'order_num': 8, 'points': 8, 'name': '8', 'label': '8',
-                'explanation': """8"""  # nopep8
+                'order_num': 8, 'points': 40, 'name': '4.0 điểm', 'label': '4.0 điểm',
+                'explanation': '40% tổng điểm'
             },
             {
-                'order_num': 9, 'points': 9, 'name': '9', 'label': '9',
-                'explanation': """9"""  # nopep8
+                'order_num': 9, 'points': 45, 'name': '4.5 điểm', 'label': '4.5 điểm',
+                'explanation': '45% tổng điểm'
             },
             {
-                'order_num': 10, 'points': 10, 'name': '10', 'label': '10',
-                'explanation': """10"""  # nopep8
+                'order_num': 10, 'points': 50, 'name': '5.0 điểm', 'label': '5.0 điểm',
+                'explanation': '50% tổng điểm'
+            },
+            {
+                'order_num': 11, 'points': 55, 'name': '5.5 điểm', 'label': '5.5 điểm',
+                'explanation': '55% tổng điểm'
+            },
+            {
+                'order_num': 12, 'points': 60, 'name': '6.0 điểm', 'label': '6.0 điểm',
+                'explanation': '60% tổng điểm'
+            },
+            {
+                'order_num': 13, 'points': 65, 'name': '6.5 điểm', 'label': '6.5 điểm',
+                'explanation': '65% tổng điểm'
+            },
+            {
+                'order_num': 14, 'points': 70, 'name': '7.0 điểm', 'label': '7.0 điểm',
+                'explanation': '70% tổng điểm'
+            },
+            {
+                'order_num': 15, 'points': 75, 'name': '7.5 điểm', 'label': '7.5 điểm',
+                'explanation': '75% tổng điểm'
+            },
+            {
+                'order_num': 16, 'points': 80, 'name': '8.0 điểm', 'label': '8.0 điểm',
+                'explanation': '80% tổng điểm'
+            },
+            {
+                'order_num': 17, 'points': 85, 'name': '8.5 điểm', 'label': '8.5 điểm',
+                'explanation': '85% tổng điểm'
+            },
+            {
+                'order_num': 18, 'points': 90, 'name': '9.0 điểm', 'label': '9.0 điểm',
+                'explanation': '90% tổng điểm'
+            },
+            {
+                'order_num': 19, 'points': 95, 'name': '9.5 điểm', 'label': '9.5 điểm',
+                'explanation': '95% tổng điểm'
+            },
+            {
+                'order_num': 20, 'points': 100, 'name': '10 điểm', 'label': '10 điểm',
+                'explanation': '100% tổng điểm'
             }
         ],
     }
